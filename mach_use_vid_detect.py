5a13371cb4db431fe713a19e9cfaf14b64054f7d
Josephine Williams <josephinew177@gmail.com>
2024-08-12 10:03:37 +0100

    Add Frame to Timecode, Add New Image Weights

--- mach_use_vid_detect.py
+++ mach_use_vid_detect.py
@@ -2,14 +2,41 @@ import cv2
 from ultralytics import YOLO
 
 # initialize video capture
-cap = cv2.VideoCapture('3_2024-07-17_09-30-33.mp4')
+cap = cv2.VideoCapture('<VIDEO PATH HERE>')
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 
 # set our confidence threshold
 conf_threshold = 0.7
 
 # initialize our trained model for image recognition
-model = YOLO("best0.pt")
+model = YOLO("best1.pt")
+white = 0
+teal = 0
+navy = 0
+
+def frames_to_timecode(frame, fps):
+    """Given the fps of a given video, will give
+    the timestamp of any specific frame.
+
+    Args:
+        frame (int): the number frame which we would
+        like to know the time stamp of
+        fps (float): number of frames per second in the
+        relevant video. 
+
+    Returns:
+        str: string giving the timestamp of the desired frame.
+    """
+    seconds = round(frame/fps)
+    
+    hr = round(seconds/3600)
+    
+    min = round(seconds/60) - hr*60
+    
+    sec = seconds - hr*3600 - min*60
+    
+    return f'the time is {hr}:{min}:{sec}'
+    
 
 # confirm the video capture is initialized
 if not cap.isOpened():
@@ -24,6 +51,7 @@ botx = 755
 length = 30 #seconds
 frame = 0
 num = 0
+
 while True:
     ret, img = cap.read()
     
@@ -32,17 +60,17 @@ while True:
         break
     
     # process image to check for any human activity every 30 seconds
-    if frame == length*fps:
+    if frame%(length*fps) == 0:
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)    
 
         # show the cropped image
-        cropped = image[topx:botx+1, topy:boty+1]
+        cropped = img[topx:botx+1, topy:boty+1]
         
         # detect color suit in cropped frame that contains machine
-        results = model(img)
+        results = model(cropped)
 
         # save what the cropped image looks like to judge results of model
-        cv2.imwrite(f'video_frames/cropped{num}.jpg', img)
+        cv2.imwrite(f'video_frames/cropped{num}.jpg', image)
         
         # UNCOMMENT PRINT STATEMENT BELOW: see more details about what our model detects in the frame.
         # print(results[0].boxes)
@@ -55,19 +83,32 @@ while True:
             for data in result.boxes.data.tolist():
                 _, _, _, _, confidence, class_id = data
                 class_id = int(class_id)
+                if count != 0:
+                    continue
                 
                 if class_id == 0:
                     count += 1
-                    print(f'navy suit spotted with {100*confidence:.2f}% confidence')
+                    # print(f'navy suit spotted with {100*confidence:.2f}% confidence')
+                    navy += 1
+                    if navy >= 2:
+                        print(f'navy suit is using the machine at image {num}')
                 if class_id == 1:
                     count += 1
-                    print(f'teal suit spotted with {100*confidence:.2f}% confidence')
+                    # print(f'teal suit spotted with {100*confidence:.2f}% confidence')
+                    teal += 1
+                    if teal >= 2:
+                        print(f'teal suit is using the machine at image {num}')
                 if class_id == 2:
                     count += 1
-                    print(f'white suit spotted with {100*confidence:.2f}% confidence')
+                    # print(f'white suit spotted with {100*confidence:.2f}% confidence')
+                    white += 1
+                    if white >= 2:
+                        print(f'white suit is using the machine at image {num}')
                 
-            if count ==0:
+            if count == 0:
                 print(f'for figure {num}, no one was detected.')
+                navy = 0
+                teal = 0
+                white = 0
         num += 1
-        frame = 0
     frame += 1
